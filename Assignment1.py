cf01c5ebcc1af2430962fa1ccca59f870ba106eb
prkgr1111 <52007649+prkgr1111@users.noreply.github.com>
2019-06-19 22:15:47 +0530

    Create Assignment1.py
    This is my first eckovation assignment in python

---
+++ Assignment1.py
@@ -0,0 +1,11 @@
+n1 = int(input("enter first number: "))
+n2 = int(input("enter second number:"))
+
+n3 = n1 + n2
+print("this is addition :",n3)
+n4 = n1*n2
+print("this is multiplication :",n4)
+n5 = n1 - n2
+print("this is subtraction :",n5)
+n6 = n1/n2
+print("this is division :",n6)
